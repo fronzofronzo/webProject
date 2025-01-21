2d0f26b249292be125a4fdf7d0e33d148708a813
Nicho <riccinicho22@gmail.com>
2025-01-21 19:06:33 +0100

    created file index.php

--- funside/template/base.php
+++ funside/template/base.php
@@ -1,22 +1,28 @@
 <!DOCTYPE html>
 <html lang="it">
 <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <title>Fun Side - Home</title>
 </head>
 <body>
     <header class="text-center text-black py-3">
         <h1>FUN SIDE</h1>
+    </header>
+    <nav>
         <div>
             <button class="btn"><i class="material-icons">person</i></button>
             <button class="btn btn-light"><i class="bi bi-person"></i>Notifiche</button>
             <button class="btn btn-light">Profilo</button>
             <button class="btn btn-light">Carrello</button>
         </div>
-    </header>
+    </nav>
    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
