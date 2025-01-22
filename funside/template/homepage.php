0a019bb510cfcf4d2ebf50e9fb2fb61ce6e3b5da
Gabriele <gabriele.fronzoni@studio.unibo.it>
2025-01-22 16:17:12 +0100

    Modified base template

--- funside/template/homepage.php
+++ funside/template/homepage.php
@@ -1,13 +1,11 @@
 <div class="row">
-		<form class="form-inline my-2 my-lg-0"><input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"><button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-		</form>
-	</div>
+		
 </div>
-<div class="row mb-2">
-	<div class="col-4 py-3">
+<div class="row mb-1">
+	<div class="col-3 text-center">
 		<img src="./upload/funko-logo-.png" class ="img-thumbnail" alt="Logo funko">
 	</div>
 </div>
 <div class="row">
-	<div class="col-4">Funko Pop</div>
+	<div class="col-3 text-center">Funko Pop</div>
 </div>
