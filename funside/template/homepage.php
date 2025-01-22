19bea44928ad286a59bb15800603c39457a6c0b0
Gabriele <gabriele.fronzoni@studio.unibo.it>
2025-01-22 16:33:09 +0100

    Modified homepage code

--- funside/template/homepage.php
+++ funside/template/homepage.php
@@ -1,11 +1,21 @@
-<div class="row">
-		
-</div>
-<div class="row mb-1">
-	<div class="col-3 text-center">
-		<img src="./upload/funko-logo-.png" class ="img-thumbnail" alt="Logo funko">
+<div class="mb-2">
+	<div class="row">
+		<h2 class="text-white">Categoria prodotto</h2>
+		<div class="col-3 text-center">
+			<img src="./upload/funko-logo-.png" class ="img-thumbnail" alt="Logo funko">
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-3 text-center">Funko Pop</div>
 	</div>
 </div>
-<div class="row">
-	<div class="col-3 text-center">Funko Pop</div>
+<div class="mb-2">
+	<div class="row">
+		<h2 class="text-white">Più venduti</h2>
+	</div>
+</div>
+<div class="mb-2">
+	<div class="row">
+		<h2 class="text-white">Più apprezzati</h2>
+	</div>
 </div>
