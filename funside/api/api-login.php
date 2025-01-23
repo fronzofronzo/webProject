56a68f98990d6a0f65a73a94168f809639f56c0d
Nicho <riccinicho22@gmail.com>
2025-01-23 11:37:03 +0100

    created api-login.php

---
+++ funside/api/api-login.php
@@ -0,0 +1,23 @@
+<?php 
+require_once 'bootstrap.php';
+
+$result['login'] = false;
+
+if (isset($_POST['username']) && isset($_POST["password"])) {
+    $login = $dbh->checkLogin($_POST["username"], $_POST["password"]);
+    if (count($login) == 0) {
+        //Login failed
+        $result["errorLogin"] = "Username e/o password non corretti";
+    } else {
+        registerLogin($login[0]);
+    }
+}
+
+if (isUserLoggedIn()) {
+    $result["login"] = true;;
+}
+
+header("'Content-Type: application/json");
+echo json_encode($result);
+
+?>
