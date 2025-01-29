3df3db7368ec899357aedacd3543ee6102b53f3c
Nicho <riccinicho22@gmail.com>
2025-01-29 18:28:05 +0100

    created and first implementation og api-order.php

---
+++ funside/api/api-order.php
@@ -0,0 +1,23 @@
+<?php
+require_once '../bootstrap.php';
+
+$result = [];
+
+if (isset($_POST['action'])) {
+    switch ($_POST['action']) {
+        case 'getorderbyuser':
+            $result['orders'] = $dbh->getOrdersByUser($_SESSION['username']);
+            if (count($result['orders']) > 0) {
+                for ($i = 0; $i < count($result['orders']); $i++) {
+                    $orderId = $result['orders'][$i]["idorder"];
+                    $result['order_details'][$orderId] = $dbh->getOrdersDetailsByOrderId($orderId);
+                }
+            }
+            break;
+        default:
+            $result["error"] = "Azione non valida";
+    }
+}
+header("Content-Type: application/json");
+echo json_encode($result);
+?>
