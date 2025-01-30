6d153732529da8d3553ff896a9a6d7b2a426a2e7
Nicho <riccinicho22@gmail.com>
2025-01-30 19:13:52 +0100

    corrected home, now is responsive

--- funside/js/index.js
+++ funside/js/index.js
@@ -22,26 +22,6 @@ async function getCategoriesData() {
 	}
 }
 
-function generateCategories(categories) {
-	let result = `
-		<div class="row">
-	`;
-	for (let i = 0; i < categories.length; i++) {
-		let category = `
-			<div class="col-6 col-md-3 col-lg-2 d-flex flex-column justify-content-center">
-				<div class="h-150 w-150 d-flex justify-content-center "><img src="${categories[i]["image"]}" class="img-thumbnail img-fluid object-fit-cover" alt="${categories[i]["type"]}" /></div>
-				<div class="">${categories[i]["type"]}</div>
-			</div>
-
-		`;
-		result += category;
-	}
-	result += `
-		</div>
-	`;
-	return result;
-}
-
 //Best sellers
 async function getBestSellersData() {
 	const url = "api/api-products.php";
@@ -90,27 +70,50 @@ async function getBestRatingsData() {
 	}
 }
 
-function generateProducts(products) {
-	let result = `
-		<div class="row">
-	`;
-	for (let i = 0; i < products.length; i++) {
-		let bestseller = `
-			<div class="col-6 col-md-3 col-lg-2 d-flex flex-column justify-content-center">
-				<div class="d-flex justify-content-center "><img src="${products[i]["image"]}" class="img-thumbnail img-fluid object-fit-cover" alt="${products[i]["name"]}" /></div>
-				<div class="">${products[i]["name"]}</div>
-				<div class="">€ ${products[i]["price"]}</div>
-				<div class="">Rating: ${products[i]["avgrating"]}</div>
-			</div>
-
-		`;
-		result += bestseller;
-	}
-	result += `
-		</div>
-	`;
-	return result;
+function generateCategories(categories) {
+    let result = `
+        <div class="row m-0">
+    `;
+    for (let i = 0; i < categories.length; i++) {
+        let category = `
+            <div class="col-6 col-sm-4 col-md-3 col-lg-2 d-flex flex-column justify-content-center">
+                <div class="h-150 w-150 d-flex justify-content-center">
+                    <img src="${categories[i]["image"]}" class="img-thumbnail img-fluid object-fit-cover" alt="${categories[i]["type"]}" />
+                </div>
+                <div class="text-center">${categories[i]["type"]}</div>
+            </div>
+        `;
+        result += category;
+    }
+    result += `
+        </div>
+    `;
+    return result;
 }
+
+function generateProducts(products) {
+    let result = `
+        <div class="row m-0">
+    `;
+    for (let i = 0; i < products.length; i++) {
+        let bestseller = `
+            <div class="col-6 col-sm-4 col-md-3 col-lg-2 d-flex flex-column justify-content-center">
+                <div class="d-flex justify-content-center">
+                    <img src="${products[i]["image"]}" class="img-thumbnail img-fluid object-fit-cover" alt="${products[i]["name"]}" />
+                </div>
+                <div class="text-center">${products[i]["name"]}</div>
+                <div class="text-center">€ ${products[i]["price"]}</div>
+                <div class="text-center">Rating: ${products[i]["avgrating"]}</div>
+            </div>
+        `;
+        result += bestseller;
+    }
+    result += `
+        </div>
+    `;
+    return result;
+}
+
 
 getCategoriesData();
 getBestSellersData();
