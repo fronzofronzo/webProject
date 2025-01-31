dbf18aea966806acf8fa4e717c7f1b625e4307ed
Gabriele <gabriele.fronzoni@studio.unibo.it>
2025-01-31 15:00:52 +0100

    Modified product page

--- funside/template/product-view.php
+++ funside/template/product-view.php
@@ -22,56 +22,51 @@ function generateStars($rating) {
 			<small class="text-body-secondary"><?php echo $product["brand"]?></small>
 		</h2>
 		<div class="row">
-			<div class="col-6 text-center">
-				<img src="<?php echo $product["image"]?>" alt="<?php echo $product["name"]?>"> 
+			<div class="col-12 col-md-6 text-center mb-3">
+				<img src="./upload/<?php echo $product["image"]?>" class="object-fit-cover border rounded" alt="<?php echo $product["name"]?>"> 
 			</div>
-			<div class="col-6 col-md-3 text-center">
-				<h3><?php echo $product["type"]?></h3>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-6"></div>
-			<div class="col-6 col-md-3 text-center"><p><?php echo generateStars($product["avgrating"]); ?></p></div>
-		</div>
-		<div class="row">
-			<div class="col-6"></div>
-			<div class="col-6 col-md-3 text-center"><h4>€ <?php echo $product["price"]?></h4></div>
-		</div>
-		<div class="row">
-			<div class="col-2 col-md-6"></div>
-			<div class="d-inline-block col-3">
-				<label for="quantity" class="form-label fw-bold">Quantità:</label>
-				<select class="form-select border-primary rounded-3" id="quantity">
-					<option value="1" selected>1</option>
-					<option value="2">2</option>
-					<option value="3">3</option>
-					<option value="4">4</option>
-					<option value="5">5</option>
-				</select>
-			</div>
-			<div class="col-3 d-grid" >
-				<button class="fa-solid fa-cart-plus border-primary rounded-3 btn btn-primary d-flex mt-auto ms-auto">
-					<div>Aggiungi al carrello</div>
-				</button> 
+			<div class="col-12 col-md-6 text-center">
+				<div class="row mb-3">
+					<h3><?php echo $product["type"]?></h3>
+				</div>
+				<div class="row mb-3 text-center"><p><?php echo generateStars($product	["avgrating"]); ?></p>
+				</div>
+				<div class="row mb-3 text-center"><h4>€ <?php echo $product["price"]?></h4></div>
+				<div class="row mb-3 ">
+					<label for="quantity" class="form-label fw-bold">Quantità:</label>
+					<select class="form-select border-primary rounded-3" id="quantity">
+						<option value="1" selected>1</option>
+						<option value="2">2</option>
+						<option value="3">3</option>
+						<option value="4">4</option>
+						<option value="5">5</option>
+					</select>
+				</div>
+				<div class="row" >
+					<button class=" border-primary rounded-3 btn btn-primary " >
+						<i class="fa-solid fa-cart-plus me-3"></i>
+						<div >Aggiungi al carrello</div>
+					</button> 
+				</div>
 			</div>
 		</div>
-	</section>
-	<section class="container-fluid">
-	<div class="tabs">
-			<div class="tab-buttons">
-				<button class="tab-button active" data-tab="description">Descrizione</button>
-				<button class="tab-button" data-tab="reviews">Recensioni</button>
-			</div>
-		<div class="tab-content">
-			<div class="tab-pane active" id="description">
-				<!-- Contenuto della descrizione -->
-				<p><?php echo $product["description"]?></p>
-			</div>
-			<div class="tab-pane" id="reviews">
-				<!-- Contenuto delle recensioni -->
-				<p>Queste sono le recensioni degli utenti.</p>
+		</section>
+		<section class="container-fluid">
+		<div class="tabs">
+				<div class="tab-buttons">
+					<button class="tab-button active" data-tab="description">Descrizione</button>
+					<button class="tab-button" data-tab="reviews">Recensioni</button>
+				</div>
+			<div class="tab-content">
+				<div class="tab-pane active" id="description">
+					<!-- Contenuto della descrizione -->
+					<p><?php echo $product["description"]?></p>
+				</div>
+				<div class="tab-pane" id="reviews">
+					<!-- Contenuto delle recensioni -->
+					<p>Queste sono le recensioni degli utenti.</p>
+				</div>
 			</div>
 		</div>
-    </div>
-	</section>
+		</section>
 </main>
