7c59ee48a4e8824c2271c22e1f87d00720c0bc07
Gabriele <gabriele.fronzoni@studio.unibo.it>
2025-02-01 16:47:39 +0100

    Modified single product in cart

--- funside/js/cart.js
+++ funside/js/cart.js
@@ -27,12 +27,12 @@ async function getProductsInCart() {
         console.log(div);
         let result = "";
         for(let i=0; i<products.length; i++) {
-            let html = `<div class="row text-center border mx-2 my-2 p-3 rounded cartElement">
+            let html = `<div class="row text-center mx-2 my-2 p-3 cartElement">
                 <div class='col-12 col-md-6'>
                     <h3>${products[i]["name"]}</h3>
                     <img src="./upload/${products[i]["image"]}" class="border rounded homeImage" alt="${products[i]["name"]}">
                 </div>
-                <div class = "col-12 col-md-6">
+                <div class = "col-12 col-md-6 align-items-center">
                     <p>Pezzi: ${products[i]["quantity"]}</p>
                     <p class="fw-bold">${products[i]["quantity"]*products[i]["price"]}</p>
                 </div>
