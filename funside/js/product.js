a2854a340c0789b8ab5a91f9d550a471df1cd5c6
Gabriele <gabriele.fronzoni@studio.unibo.it>
2025-02-02 14:39:52 +0100

    Deleted duplicated tag main

--- funside/js/product.js
+++ funside/js/product.js
@@ -46,6 +46,8 @@ function init() {
             }
         });
     });
+
+    
 }
 
 async function getDataReviews() {
--- funside/template/product-view.php
+++ funside/template/product-view.php
@@ -15,57 +15,55 @@ function generateStars($rating) {
 
 ?>
 
-<main>
-	<section class="container-fluid">
-		<h2 class="mb-3">
-			<?php echo $product["name"]?>
-			<small class="text-body-secondary"><?php echo $product["brand"]?></small>
-		</h2>
-		<div class="row">
-			<div class="col-12 col-md-6 text-center mb-3">
-				<img src="./upload/<?php echo $product["image"]?>" class=" border rounded" alt="<?php echo $product["name"]?>"> 
+<section class="container-fluid">
+	<h2 class="mb-3">
+		<?php echo $product["name"]?>
+		<small class="text-body-secondary"><?php echo $product["brand"]?></small>
+	</h2>
+	<div class="row">
+		<div class="col-12 col-md-6 text-center mb-3">
+			<img src="./upload/<?php echo $product["image"]?>" class=" border rounded" alt="<?php echo $product["name"]?>"> 
+		</div>
+		<div class="col-12 col-md-6 text-center">
+			<div class="row mb-3">
+				<h3><?php echo $product["type"]?></h3>
 			</div>
-			<div class="col-12 col-md-6 text-center">
-				<div class="row mb-3">
-					<h3><?php echo $product["type"]?></h3>
-				</div>
-				<div class="row mb-3 text-center"><p><?php echo generateStars($product	["avgrating"]); ?></p>
-				</div>
-				<div class="row mb-3 text-center"><h4>€ <?php echo $product["price"]?></h4></div>
-				<div class="row mb-3 ">
-					<label for="quantity" class="form-label fw-bold">Quantità:</label>
-					<select class="form-select border-primary rounded-3" id="quantity">
-						<option value="1" selected>1</option>
-						<option value="2">2</option>
-						<option value="3">3</option>
-						<option value="4">4</option>
-						<option value="5">5</option>
-					</select>
-				</div>
-				<div class="row" >
-					<button class=" border-primary rounded-3 btn btn-primary " >
-						<i class="fa-solid fa-cart-plus me-3"></i>
-						<div >Aggiungi al carrello</div>
-					</button> 
-				</div>
+			<div class="row mb-3 text-center"><p><?php echo generateStars($product	["avgrating"]); ?></p>
+			</div>
+			<div class="row mb-3 text-center"><h4>€ <?php echo $product["price"]?></h4></div>
+			<div class="row mb-3 ">
+				<label for="quantity" class="form-label fw-bold">Quantità:</label>
+				<select class="form-select border-primary rounded-3" id="quantity">
+					<option value="1" selected>1</option>
+					<option value="2">2</option>
+					<option value="3">3</option>
+					<option value="4">4</option>
+					<option value="5">5</option>
+				</select>
+			</div>
+			<div class="row" >
+				<button class=" border-primary rounded-3 btn btn-primary " >
+					<i class="fa-solid fa-cart-plus me-3"></i>
+					<div >Aggiungi al carrello</div>
+				</button> 
 			</div>
 		</div>
-	</section>
-	<section class="container-fluid">
-		<div class="tabs">
-			<div class="tab-buttons">
-				<button class="tab-button active" data-tab="description">Descrizione</button>
-				<button class="tab-button" data-tab="reviews">Recensioni</button>
+	</div>
+</section>
+<section class="container-fluid">
+	<div class="tabs">
+		<div class="tab-buttons">
+			<button class="tab-button active" data-tab="description">Descrizione</button>
+			<button class="tab-button" data-tab="reviews">Recensioni</button>
+		</div>
+		<div class="tab-content">
+			<div class="tab-pane active" id="description">
+				<!-- Contenuto della descrizione -->
+				<p><?php echo $product["description"]?></p>
 			</div>
-			<div class="tab-content">
-				<div class="tab-pane active" id="description">
-					<!-- Contenuto della descrizione -->
-					<p><?php echo $product["description"]?></p>
-				</div>
-				<div class="tab-pane " id="reviews">
-					
-				</div>
+			<div class="tab-pane " id="reviews">
+				
 			</div>
 		</div>
-	</section>
-</main>
+	</div>
+</section>
