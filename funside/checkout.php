f9084b3f0d0648b69f1c0bf605748ec205395aca
Gabriele <gabriele.fronzoni@studio.unibo.it>
2025-02-03 08:56:47 +0100

    Initalized checkout view.

---
+++ funside/checkout.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'bootstrap.php';
+
+$templateParams["name"] = "template/checkout.php";
+//$templateParams["js"] = array("js/checkout.js");
+$templateParams["title"] = "Funside - conferma ordine";
+
+require 'template/base.php';
+?>
