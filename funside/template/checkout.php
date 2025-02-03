6b1bae4eb629206b70542f5dc062491e7dcec48a
Gabriele <gabriele.fronzoni@studio.unibo.it>
2025-02-03 09:02:16 +0100

    Initialized checkout view with dummy product.

---
+++ funside/template/checkout.php
@@ -0,0 +1,12 @@
+<section class="container-fluid">
+	<div class="row">
+		<h2>Carrello</h2>
+		<div class="border rounded border-black p-2 col-6">
+			<div>
+				<strong>Prodotto x</strong>
+				<p>Pezzi: y</p>
+				<p>Totale: z</p>
+			</div>
+		</div>
+	</div>
+</section>
