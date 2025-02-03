c3fa58e5e7f0536ef5c6d7cd322fa35a4ec5b8dc
Gabriele <gabriele.fronzoni@studio.unibo.it>
2025-02-03 09:43:02 +0100

    Initialized page section to select delivery address.

--- funside/template/checkout.php
+++ funside/template/checkout.php
@@ -4,4 +4,9 @@
 		<div class="border rounded border-black p-2 col-12 col-md-6">
 		</div>
 	</div>
+	<div class="row mx-2 border rounded border-black">
+		<div class="d-flex align-items-center justify-content-center" >
+			<i class="fa-solid fa-down-long"></i><strong class="mx-2">Scegli l'indirizzo di consegna dell'ordine</strong><i class="fa-solid fa-down-long"></i>
+		</div>
+	</div>
 </section>
