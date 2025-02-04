a037e7b29e19b8080424f256611281174be7511b
Gabriele <gabriele.fronzoni@studio.unibo.it>
2025-02-04 16:48:31 +0100

    Removed toggle menu and changed it with one button to go to catalogue.

--- funside/template/base.php
+++ funside/template/base.php
@@ -32,46 +32,7 @@
             <a href="notification.php" class="fa-solid fa-bell btn btn-light col-1" title='Notifications'></a>
             <a href="login.php" class="fa-solid fa-user btn btn-light col-1" title="Account"></a>
             <a href="cart.php" class="fa-solid fa-cart-shopping btn btn-light col-1" title="Cart"></a>
-            <div class="col-8"></div>
-            <button class="navbar-toggler col-1" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
-                aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar"
-                aria-labelledby="offcanvasNavbarLabel">
-                <div class="offcanvas-header">
-                    <h2 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h2>
-                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                </div>
-                <div class="offcanvas-body">
-                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="./catalogue.php">Catalogo</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                                aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                    <form class="d-flex mt-3" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
-                </div>
-            </div>
+            <a href="catalogue.php" class="fa-solid fa-bars btn btn-light col-1" title="Catalogue"></a>
         </div>
     </nav>
     <main>
