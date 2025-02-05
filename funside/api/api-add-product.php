1e07b6570ef68632feeb859641f171ea265d6b4c
Nicho <riccinicho22@gmail.com>
2025-02-05 11:55:21 +0100

    mod image

--- funside/api/api-add-product.php
+++ funside/api/api-add-product.php
@@ -34,9 +34,6 @@ if ($image["error"] !== UPLOAD_ERR_OK) {
 
 // Puoi continuare a gestire l'upload dell'immagine come fai normalmente
 
-
-$image = $_FILES["image"]; // Correctly get the uploaded image file
-
 if (!is_numeric($price) || $price <= 0) {
     $result["message"] = "Prezzo non valido";
     echo json_encode($result);
